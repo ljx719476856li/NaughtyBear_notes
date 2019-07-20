d02739218d25f9cced7b3aa9b94ba6572cb7aafc
NaughtyBear <719476856@qq.com>
2019-07-20 22:41:59 +0800

    ini
    ini

---
+++ Shader Learning/Shader_01/Assets/rotations.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotations : MonoBehaviour {
+
+    public Transform target;
+    public float smooth = 5.0F;
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * smooth);
+    }
+}
