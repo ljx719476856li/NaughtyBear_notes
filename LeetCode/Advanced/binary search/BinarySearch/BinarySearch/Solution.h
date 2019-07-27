d85d24412aab228f28b3fd6d44e8a5b763b279a0
ljx719476856li <719476856@qq.com>
2019-07-28 00:07:20 +0800

    init Binary Search
    init Binary Search

---
+++ LeetCode/Advanced/binary search/BinarySearch/BinarySearch/Solution.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int searchInsert(vector<int>& nums, int target) 
+	{
+		if (nums.back() < target) return nums.size();
+		int left = 0, right = nums.size() - 1;
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) return mid;
+			else if (nums[mid] < target) left = mid + 1;
+			else right = mid;
+		}
+		return right;
+	}
+};
