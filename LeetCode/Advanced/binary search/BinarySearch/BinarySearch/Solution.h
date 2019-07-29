5b125347f0832d82869e8685c59687d3e4f50ecc
ljx719476856li <719476856@qq.com>
2019-07-29 23:27:45 +0800

    34. commit
    34. commit

--- LeetCode/Advanced/binary search/BinarySearch/BinarySearch/Solution.h
+++ LeetCode/Advanced/binary search/BinarySearch/BinarySearch/Solution.h
@@ -19,4 +19,26 @@ public:
 		}
 		return right;
 	}
+
+	//在排序数组中查找元素的第一个和最后一个位置
+	vector<int> searchRange(vector<int>& nums, int target) 
+	{
+		vector<int> res(2, -1);
+		int left = 0, right = nums.size() - 1;
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] < target) left = mid + 1;
+			else right = mid;
+		}
+		if (nums[right] != target) return res;
+		res[0] = right;
+		right = nums.size();
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] <= target) left = mid + 1;
+			else right = mid;
+		}
+		res[1] = left - 1;
+		return res;
+	}
 };
