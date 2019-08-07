7bcdf8e5c4e502f1b6a7398e924f504c4d6b2fb1
NaughtyBear <719476856@qq.com>
2019-08-07 21:42:08 +0800

    MaxGap问题
    MaxGap问题

---
+++ LeetCode/Advanced/NCZG_01/MaxGap/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+/*
+思路：求相邻俩数的最大值
+*/
+class Solution
+{
+public:
+	void maxGap();
+	void bucket();
+
+};
+
+void maxGap();
+void bucket();
+
+int main()
+{
+
+	return -1;
+}
