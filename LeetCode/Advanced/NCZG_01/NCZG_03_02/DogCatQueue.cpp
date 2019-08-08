0883724b8286ea9ceae679a4505748ae7d00b398
ljx719476856li <719476856@qq.com>
2019-08-09 00:15:07 +0800

    猫狗队列
    猫狗队列

--- LeetCode/Advanced/NCZG_01/NCZG_03_02/DogCatQueue.cpp
+++ LeetCode/Advanced/NCZG_01/NCZG_03_02/DogCatQueue.cpp
@@ -19,11 +19,19 @@ class Cat :public Pet
 {
 public:
 	Cat() { m_sType = "Cat"; }
+	Cat(const Cat& cat)
+	{
+		m_sType = cat.m_sType;
+	}
 };
 class Dog : public Pet
 {
 public:
 	Dog() { m_sType = "Dog"; }
+	Dog(const Dog& dog)
+	{
+		m_sType = dog.m_sType;
+	}
 };
 
 class PetEnterQueue
@@ -51,9 +59,9 @@ public:
 	DogCatQueue():m_iCount(0) {}
 
 	void Add(Pet pet); //将cat类或dog类的 实例放入队列中
-	Pet* PollAll(); //将队列中所有的实例按照进队列 的先后顺序依次弹出
-	Dog* PollDog(); //将队列中dog类的实例按照 进队列的先后顺序依次弹出
-	Cat* PollCat(); // 将队列中cat类的实 例按照进队列的先后顺序依次弹出
+	Pet PollAll(); //将队列中所有的实例按照进队列 的先后顺序依次弹出
+	Dog PollDog(); //将队列中dog类的实例按照 进队列的先后顺序依次弹出
+	Cat PollCat(); // 将队列中cat类的实 例按照进队列的先后顺序依次弹出
 
 	bool isEmpty(); //检查队列中是 否还有dog或cat的实例
 	bool isDogEmpty(); //检查队列中是否有dog 类的实例
@@ -71,31 +79,30 @@ void DogCatQueue::Add(Pet pet)
 		return;
 }
 
-Pet* DogCatQueue::PollAll()
+Pet DogCatQueue::PollAll()
 {
-	Pet* res;
+	Pet res;
 	if (!m_cat.empty() && !m_dog.empty())
 	{
 		if (m_cat.front().GetCount() < m_dog.front().GetCount())
 		{
-			res = m_cat.front().GetPet();
+			res = Cat(*static_cast<Cat*>(m_cat.front().GetPet()));
+			//res = m_cat.front().GetPet();
 			m_cat.pop();
 		}
 		else
 		{
-			res = m_dog.front().GetPet();
+			res = Dog(*static_cast<Dog*>(m_dog.front().GetPet()));
 			m_dog.pop();
 		}
 	}
 	else if (!m_cat.empty())
 	{
-		res = m_cat.front().GetPet();
-		m_cat.pop();
+		res = PollCat();
 	}
 	else if (!m_dog.empty())
 	{
-		res = m_dog.front().GetPet();
-		m_dog.pop();
+		res = PollDog();
 	}
 	else
 		throw "err, queue is empty!";
@@ -103,22 +110,26 @@ Pet* DogCatQueue::PollAll()
 	return res;
 }
 
-Cat* DogCatQueue::PollCat()
+Cat DogCatQueue::PollCat()
 {
 	if (isCatEmpty())
 		throw "err, queue is empty!";
 	
-	Cat* cat = static_cast<Cat*>(m_cat.front().GetPet());
+	Cat res(*static_cast<Cat*>(m_cat.front().GetPet()));
+	//Dog* dog = static_cast<Dog*>(m_dog.front().GetPet());
 	m_cat.pop();
-	return cat;
+	return res;
 }
 
-Dog* DogCatQueue::PollDog()
+Dog DogCatQueue::PollDog()
 {
 	if (isDogEmpty())
 		throw "err, queue is empty!";
 
-	Dog* dog = static_cast<Dog*>(m_dog.front().GetPet());
+	Dog res(*static_cast<Dog*>(m_dog.front().GetPet()));
+	//Dog* dog = static_cast<Dog*>(m_dog.front().GetPet());
+	m_dog.pop();
+	return res;
 }
 	
 bool DogCatQueue::isEmpty()
@@ -160,14 +171,15 @@ int main()
 
 	test.Add(*dog1);
 	test.Add(*cat1);
-	test.Add(*dog2);
 	test.Add(*cat2);
-	test.Add(*dog3);
+	test.Add(*cat2);
+	test.Add(*cat3);
 	test.Add(*cat3);
 
 	while (!test.isEmpty())
 	{
-		string type = test.PollAll()->GetType();
+		string type = test.PollAll().GetType();
+		cout << type.data() << " ";
 	}
 
 	return 0;
